ad44a3900e98447bd83c13230149cc36f145e5ba
Akseli Oksanen <akseli.oksanen@gmail.com>
2026-08-17 08:42:33 +0300

    Create MT-Tili website prototype

---
+++ src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+
+declare namespace JSX {
+  interface Element extends React.ReactElement {}
+}
